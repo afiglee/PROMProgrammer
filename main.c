180ade972bbd03237ba8acba00cec05b00b7cd61
Dmitriy Fitisov <dmitriy@radier.ca>
2024-08-16 09:05:36 -0400

    Work in progreess

---
+++ main.c
@@ -0,0 +1,535 @@
+/*
+ * File:   main.c
+ * Author: dmitriy
+ *
+ * Created on September 22, 2019, 5:38 PM
+ */
+
+//Frequency 7.3728MHz //11.0592 MHz
+
+
+
+#include "config.h"
+#include <xc.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+//#include <pic18f26k22.h>
+
+#define PROG_OFF PORTCbits.RC5 = 0;
+#define PROG_ON PORTCbits.RC5 = 1;
+#define CS_ON PORTCbits.RC2 = 1;
+#define CS_OFF PORTCbits.RC2 = 0;
+
+#define NOT_HEX(A) ((A < '0' || A > 'f') || \
+        (A > 'F' && A < 'a') || \
+        (A > '9' && A < 'A'))
+
+#define PRINT(A) while(PIE1bits.TX1IE); \
+                 strcpy((char*) _buf_out.buffer, A); \
+                 _buf_out.seek = 0; \
+                 TXSTA1bits.TXEN = 1; \
+                 PIE1bits.TX1IE = 1;
+            
+#define ENABLE_TX TXSTA1bits.TXEN = 1; \
+                  PIE1bits.TX1IE = 1;
+
+#define HEX2CHAR(hex,ch) if (hex >= 0x0A) {\
+        ch = 'A' + (hex - 0x0A);\
+    } else {\
+        ch = '0' + hex;\
+    }
+
+#define DAC_STEP 2
+
+// With those values @ 11.0592MHz and prescaler 1:256 
+// timer period would be approximately 50 msec
+//const uint8_t TIMER_LOW = 0x00;
+//const uint8_t TIMER_HIGH = 0x79; 
+
+// With those values @ 7.3728MHz and prescaler 1:256 
+// timer period would be approximately 50 msec
+const uint8_t TIMER_LOW = 0x00;
+const uint8_t TIMER_HIGH = 0xA6; 
+
+const uint8_t FLAG_DELAY_START = 0x01;
+const uint8_t FLAG_DELAY_STOP = 0x02;
+const uint8_t FLAG_CMD_IN = 0x04;
+const uint8_t FLAG_READY = 0x08;
+
+#define MODE_RE3 1
+#define MODE_RT4 2
+
+const uint8_t ERR = 0;
+const uint8_t OK = 1;
+
+#define CMD_LEN  54
+
+uint8_t _mode = MODE_RT4;
+uint8_t _addr = 0;
+uint8_t _flags = 0;
+uint8_t _delay_ms = 0;
+
+typedef struct s_io_buf {
+    char buffer[CMD_LEN];
+    uint8_t seek;
+} io_buf;
+
+io_buf _buf_in;
+io_buf _buf_out;
+
+char _cmd_buf[CMD_LEN];
+
+void __interrupt() isr(void)
+{
+    if (INTCONbits.TMR0IF) {
+        INTCONbits.TMR0IF = 0;
+        TMR0H = TIMER_HIGH;
+        TMR0L = TIMER_LOW;
+        if (_flags & FLAG_DELAY_START) {
+            if (_delay_ms) {
+                --_delay_ms;
+            } 
+            if (_delay_ms == 0) {
+                _flags &= ~FLAG_DELAY_START;
+                _flags |= FLAG_DELAY_STOP;
+                T0CONbits.TMR0ON = 0;
+            }        
+        }
+        return;
+    } else if (PIR1bits.TX1IF) {
+        PIR1bits.TX1IF = 0;
+        if (_buf_out.buffer[_buf_out.seek] == 0) {
+            //TXSTA1bits.TXEN = 0;
+            PIE1bits.TX1IE = 0;
+        } else {
+            TXREG1 = _buf_out.buffer[_buf_out.seek++];
+        }
+    } /*else*/ if (PIR1bits.RC1IF) {
+        uint8_t data = RCREG1;
+        TXREG1 = data;
+        if (_flags & FLAG_CMD_IN) {
+            // if we have not processed data yet
+            // skip characters until we finish processing
+            return;
+        }
+        _buf_in.buffer[_buf_in.seek++] = data;
+        if (data == 0x0A /*0x0D*/ || _buf_in.seek == CMD_LEN) {
+            _flags |= FLAG_CMD_IN;
+            _buf_in.buffer[_buf_in.seek] = 0;
+            _buf_in.seek = 0;
+            strcpy((char*) &_cmd_buf[0], (char*) &_buf_in.buffer[0]);
+        }
+    }
+}
+
+void start_timer()
+{
+    _delay_ms = 2;
+    _flags |= FLAG_DELAY_START;
+    T0CONbits.TMR0ON = 1;
+}
+
+void wait_timer() 
+{
+    while((_flags & FLAG_DELAY_STOP) == 0);
+    _flags &= ~FLAG_DELAY_STOP;
+}
+
+void set_addr(uint8_t new_addr) {
+    _addr = new_addr;
+    if (new_addr & 0x80) {
+        PORTCbits.RC1 = 0;
+    } else {
+        PORTCbits.RC1 = 1;
+    }
+    if (new_addr & 0x40) {
+        PORTCbits.RC0 = 0;
+    } else {
+        PORTCbits.RC0 = 1;
+    }
+    PORTB = (new_addr ^ 0x3F);
+}
+
+void init()
+{
+    memset(&_buf_in, 0, sizeof(_buf_in));
+    memset(&_buf_out, 0, sizeof(_buf_out));
+    
+    ANSELA = 0;
+    ANSELB = 0;
+    ANSELC = 0;
+    TRISA = 0;
+    TRISB = 0xC0;
+    TRISC = 0xC8;
+    PROG_OFF
+    CS_OFF //PORTC = 0x08; //inverse output
+    set_addr(0);
+    //init timer
+    // 1msec interrupt
+    TMR0H = 0xFA;
+    TMR0L = 0xAB;
+    RCONbits.IPEN = 0;
+    INTCON = 0xE0;
+    T0CON = 0;
+    T0CONbits.T0PS = 1;//7;
+    T0CONbits.PSA = 0; //0x88;
+    
+    //init RS232
+    //115200 Baud @ 11.0592 MHz
+    //SPBRG1 = 5;
+    // BRGH = 1, BRG16 = 0
+    //BAUDCON1bits.BRG16 = 0;
+    
+    //115200 Baud @ 7.3728 MHz
+    SPBRG1 = 3;
+    // BRGH = 1, BRG16 = 0
+    BAUDCON1bits.BRG16 = 0;
+    
+    TXSTA1bits.BRGH = 1;
+    TXSTA1bits.SYNC = 0;
+    RCSTA1bits.SPEN = 1;
+    RCSTA1bits.CREN = 1;
+    PIE1bits.TX1IE = 0;
+    PIE1bits.RC1IE = 1;
+    
+    //DAC
+    VREFCON2bits.DACR = 0;
+    VREFCON1bits.DACPSS = 0; //VREF = VDD
+    VREFCON1bits.DACLPS = 1;
+    VREFCON1bits.DACNSS = 0;
+    VREFCON1bits.DACEN = 1; //Enable DAC
+    VREFCON1bits.DACOE = 1;
+    
+}
+
+void lower_voltage()
+{
+    VREFCON2bits.DACR = 0;
+}
+
+uint8_t step_up_voltage()
+{
+    uint8_t dac = VREFCON2bits.DACR;
+    dac += DAC_STEP;
+    if ((dac & 0x1F) < VREFCON2bits.DACR) {
+        return 0;
+    }
+    VREFCON2bits.DACR = dac;
+    return 1;
+}
+
+void set_bit(uint8_t data) 
+{
+    if (data & 0x01) {
+        PORTAbits.RA3 = 1;
+    } else {
+        PORTAbits.RA3 = 0;
+    }
+    if (data & 0x02) {
+        PORTAbits.RA4 = 1;
+    } else {
+        PORTAbits.RA4 = 0;
+    }
+    if (data & 0x04) {
+        PORTAbits.RA5 = 1;
+    } else {
+        PORTAbits.RA5 = 0;
+    }
+}
+
+uint8_t write_byte(uint8_t addr, uint8_t bits)
+{
+    _flags &= ~FLAG_READY;
+    uint8_t ret = 0xFF;
+    set_addr(addr);
+    //uint8_t seek = 0;
+    for (uint8_t tt = 0; tt < 8; tt++) {
+        if ((bits & (1<<tt))) {
+            lower_voltage();
+            // write bit
+            set_bit(tt);
+            //CS activate
+            if (_mode == MODE_RT4) {
+                CS_ON //PORTCbits.RC2 = 0;
+            }
+            //turn on Programming voltage
+            //PORTCbits.RC5 = 1;
+            uint8_t not_good = 1;        
+            while(not_good) {
+                PROG_ON
+                
+                // 100 ms pulse
+                start_timer();
+                wait_timer();
+                
+                PROG_OFF
+                // check bit
+                uint8_t rd = PORTCbits.RC3;
+                if (rd == 0) {
+                    //ok, all good, set bit
+                    ret &= ~(1<<tt);
+                    break;
+                }
+                not_good = step_up_voltage();
+            }
+            if (_mode == MODE_RT4) {
+                CS_OFF
+            }
+        }
+    }
+    lower_voltage();
+    _flags |= FLAG_READY;
+    return ret;
+}
+
+uint8_t read_byte(uint8_t address) {
+    
+    _flags &= ~FLAG_READY;
+    uint8_t ret = 0x00;
+    set_addr(address);
+    
+    for (uint8_t tt = 0; tt < 8; tt++) {
+        uint8_t seek = 0;
+        set_bit(tt);
+        //CS activate
+        CS_ON //PORTCbits.RC2 = 0;
+        while (seek++ < 10);        
+        /*asm("nop"); 
+        asm("nop");
+        asm("nop");*/
+        uint8_t rd = PORTCbits.RC3;
+        if (rd == 0) {
+            //ok, all good, set bit
+            ret |= (1<<tt);
+        }
+        CS_OFF
+    }
+    _flags |= FLAG_READY;
+    return ret;    
+}
+
+void print_help(const char *msg)
+{
+    PRINT(msg);
+}
+
+void print_mode() {
+    switch (_mode) {
+        case MODE_RT4:
+            PRINT("rt4\n");
+            break;
+        default:
+            PRINT("re3\n");
+    }
+}
+
+uint8_t hex2uint8(char* p)
+{
+    uint8_t val = 0;
+    uint8_t steps = 2;
+    while(steps--) {
+        val <<= 4;
+        if (*p <= '9') {
+            val |= *p - '0';
+        } else {
+            *p &= 0x5F;
+            val |= 0xA + (*p - 'A'); 
+        }
+        p++;
+    }
+    return val;
+}
+
+void uint8_2hex(uint8_t data, char *p)
+{
+    
+    uint8_t hex = data >> 4;
+    /*if (hex >= 0x0A) {
+        *p = 'A' + (hex - 0x0A);
+    } else {
+        *p = '0' + hex;
+    }*/
+    HEX2CHAR(hex,*p);
+    hex = data & 0x0F;
+    /*if (hex >= 0x0A) {
+        *p = 'A' + (hex - 0x0A);
+    } else {
+        *p = '0' + hex;
+    }*/
+    HEX2CHAR(hex,*(p+1));
+}
+
+uint8_t check_hex(char *p) {
+    if (NOT_HEX(*p) || (NOT_HEX(*(p+1)))) {
+        return 0;
+    }
+    return 1;
+}
+
+void print_result(uint8_t result, uint8_t data)
+{
+    _buf_out.seek = 0;
+    if (result == OK) {
+        _buf_out.buffer[_buf_out.seek++] = 'O';
+        _buf_out.buffer[_buf_out.seek++] = 'K';
+        _buf_out.buffer[_buf_out.seek++] = ' ';
+    } else { //ERR
+        _buf_out.buffer[_buf_out.seek++] = 'E';
+        _buf_out.buffer[_buf_out.seek++] = 'R';
+        _buf_out.buffer[_buf_out.seek++] = ' ';
+    }
+    uint8_t hi = (data & 0xF0) >> 4;
+    if (hi > 9) {
+        _buf_out.buffer[_buf_out.seek++] = 'A' + (hi - 10);
+    } else {
+        _buf_out.buffer[_buf_out.seek++] = '0' + hi;
+    }
+    data &= 0x0F;
+    if (data > 9) {
+        _buf_out.buffer[_buf_out.seek++] = 'A' + (data - 10);
+    } else {
+        _buf_out.buffer[_buf_out.seek++] = '0' + data;
+    }
+    _buf_out.buffer[_buf_out.seek++] = '\n';
+    _buf_out.buffer[_buf_out.seek] = 0;
+    _buf_out.seek = 0;
+    ENABLE_TX
+}
+
+void on_cmd() {
+    while ((_flags & FLAG_READY) == 0);
+    uint8_t addr = 0;
+    if (strncmp(_cmd_buf, "m", 1) == 0) {
+        if (_mode == MODE_RT4) {
+            _mode = MODE_RE3;
+        } else {
+            _mode = MODE_RT4;
+        }
+        print_mode();
+    } else if (strncmp(_cmd_buf, "w ", 2) == 0) {
+        //write byte
+        if (strlen(_cmd_buf) < 7) {
+            print_help("write - no address\n");
+            return;
+        }
+        if (!check_hex(&_cmd_buf[2])) {
+            print_help("write - non hex address\n");
+            return;            
+        }
+        if (!check_hex(&_cmd_buf[5])) {
+            print_help("write - non hex data\n");
+            return;            
+        }
+        addr = hex2uint8(&_cmd_buf[2]);
+        uint8_t data = hex2uint8(&_cmd_buf[5]);
+        uint8_t ret = write_byte(addr, data);
+        //TODO print success or error
+        if (ret == data) {
+            print_result(OK, data);
+        } else {
+            print_result(ERR, data);
+        }
+    } else if (strncmp(_cmd_buf, "r ", 2) == 0) {
+        if (strlen(_cmd_buf) < 5) {
+            print_help("read no address\n");
+            return;
+        }
+        if (!check_hex(&_cmd_buf[2])) {
+            print_help("read - no hex address\n");
+            return;            
+        }
+        addr = hex2uint8(&_cmd_buf[2]);
+        uint8_t data = read_byte(addr);
+        //TODO print result
+        print_result(OK, data);
+    } else if (strncmp(_cmd_buf, "R ", 2) == 0) {
+        // wait for serial
+        addr = 0;
+        uint8_t cycles = 0x02;
+        if (_mode == MODE_RT4) {
+            cycles = 0x10;
+        }
+        for (size_t cycle = 0; cycle < cycles; cycle++) {
+            char mem[54];
+            HEX2CHAR(cycle,mem[0]);
+            //mem[0] = cycle + '0';
+            mem[1] = '0';
+            mem[2] = ' ';
+            mem[3] = ' ';
+            uint8_t seek = 4;
+            for (size_t next = 0; next < 16; next++) {
+                uint8_t data = read_byte(addr++);
+                if (_mode == MODE_RT4) {
+                    mem[seek++] = 'x';
+                    HEX2CHAR((data & 0x0F),mem[seek]);
+                    seek++;
+                } else {
+                    uint8_2hex(data, &mem[seek]);
+                    seek += 2;
+                }
+                mem[seek++] = ' ';
+            }
+            mem[seek] = 0;
+            PRINT(&mem[0]);
+            PRINT("\n");
+        }
+        
+    } else {
+        print_help("Invalid command\n");
+    }
+}
+
+void main(void) {
+    init();
+    lower_voltage();
+    uint8_t old_flags = 0;
+    
+    
+    _flags |= FLAG_READY;
+    strcpy((char*) &_buf_out.buffer[0], "testing.. "__TIME__"\n\r");
+    TXSTA1bits.TXEN = 1;
+    PIE1bits.TX1IE = 1;
+    
+    while(1) {
+        PORTAbits.RA0 = !PORTAbits.RA0;
+        if (_flags & FLAG_CMD_IN) {
+            _flags &= ~FLAG_CMD_IN;
+            on_cmd();
+        }
+            PROG_ON
+            // 100 ms pulse
+            start_timer();
+            wait_timer();
+            PROG_OFF
+            start_timer();
+            wait_timer();
+        /*if (TXSTA1bits.TXEN == 0) {
+            buf_out.seek = 0;
+            TXSTA1bits.TXEN = 1;
+        }*/
+        /*
+        if (_flags & FLAG_DELAY_STOP) {
+            PORTCbits.RC2 = 0;
+            _flags &= ~FLAG_DELAY_STOP;
+            uint16_t kk = 0;
+            while (kk < 4096) {
+                kk++;
+            }
+        } else if ((_flags & (FLAG_DELAY_START)) == 0){
+            PORTCbits.RC2 = 1;
+            start_timer();
+            wait_timer();
+        }*//*
+            PORTCbits.RC2 = 1;
+            start_timer();
+            wait_timer();
+            PORTCbits.RC2 = 0;
+            uint16_t kk = 0;
+            while (kk < 4096) {
+                kk++;
+            }
+        */
+    }
+    return;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PROMProgrammer.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PROMProgrammer.X.production.hex
